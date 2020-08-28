3ca4aa9ae141e30e412f77ea42cfcb60651580c4
Yanina Hulevich <yanina_hulevich@epam.com>
2020-08-28 14:07:53 +0300

    feat: rename delivery token and env variables

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 module.exports = {
     contentstack: {
         api_key: process.env.CS_API_KEY,
-        delivery_token: process.env.CS_TOKEN,
-        environment: process.env.CS_ENV
+        delivery_token: process.env.CS_DELIVERY_TOKEN,
+        environment: process.env.CS_PRODUCTION_ENV
     }
 }
